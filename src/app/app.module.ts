6db851d53ee6703c56c54de62354d036f397cf3e
Pedro-B-Freitag <pedrobosini09@icloud.com>
2024-04-25 12:50:46 -0300

    Task 2 of the course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { WarningAlert } from './warning-alert/warning-alert.component';
 import { AppComponent } from './app.component';
 import { SuccessAlert } from './success-alert/success-alert.component';
+import { DatabindingComponent } from './databinding/databinding.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     WarningAlert,
     SuccessAlert,
-    AppComponent
+    AppComponent,
+    DatabindingComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
